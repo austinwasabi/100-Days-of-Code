c0120dc36f5955383e4e0fde28c92354ff06bdc7
Augustine Ledwaba <108596811+austinwasabi@users.noreply.github.com>
2023-11-25 23:18:20 +0200

    Create manipulating-data.py
    Learning about f-string and how to manipulate saved variables

---
+++ Exercises/manipulating-data.py
@@ -0,0 +1,17 @@
+result = 4/2
+result /= 2
+print(result)
+
+#user scores a point
+
+score = 0
+score += 1
+print(score)
+
+score -=1
+height = 1.8
+IsWinning = True
+
+#f-string 
+
+print(f"Your score is {score}, your height is {height}, you are winning is {IsWinning}")
