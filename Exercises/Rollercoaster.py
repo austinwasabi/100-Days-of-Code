0f5934f120e729644f50325e4e6c9e37148c1e3c
Augustine Ledwaba <108596811+austinwasabi@users.noreply.github.com>
2023-11-30 00:19:25 +0200

    Update Rollercoaster.py

--- Exercises/Rollercoaster.py
+++ Exercises/Rollercoaster.py
@@ -4,10 +4,10 @@ if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age?\n"))
     if age < 12:
-        print("Please pay $5.")
+        print("Please pay R5.")
     elif age <= 18:
-      print("Please pay $7.")
+      print("Please pay R7.")
     else:
-      print("Please pay $12.")
+      print("Please pay R12.")
 else:
     print("Sorry, you are too short to ride the rollercoaster.")
